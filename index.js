cb6a25122a9cb182ac65f770bd8a94fb804ce6ab
davidberget@gmail.com <davidberget@gmail.com>
2020-02-26 12:09:43 -0600

    checking ext name

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const path = require("path");
 
 module.exports = (job, settings, options, type) => {
   return new Promise((resolve, reject) => {
-    job.assets.forEach(asset => {
-      console.log(asset);
-    });
+    job.assets
+      .filter(asset => asset.type === "image")
+      .forEach(asset => {
+        console.log(path.extname(asset.dest));
+      });
 
     resolve(job);
   });
