cf2a87d035ae00c3ce9c5787fb46f5d0da721a48
davidberget@gmail.com <davidberget@gmail.com>
2020-02-26 14:31:00 -0600

    Add head request

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { name } = require("./package.json");
 const path = require("path");
 const fs = require("fs");
+var request = require("request");
 
 module.exports = (job, settings, options, type) => {
-  let ext = (options && options.ext) || "jpeg";
-
   return new Promise((resolve, reject) => {
     job.assets
       .filter(asset => asset.type === "image")
       .forEach(image => {
         if (path.extname(image.dest) === "") {
+          let ext = getContentType(image.src);
           let newFileName = `${image.dest}.${ext}`;
 
           if (fs.existsSync(image.dest)) {
@@ -27,3 +27,19 @@ module.exports = (job, settings, options, type) => {
     resolve(job);
   });
 };
+
+const regex = /([^\/]+$)/;
+
+function getContentType(src) {
+  const options = {
+    method: "HEAD",
+    headers: headers,
+    url: src
+  };
+
+  let contentType = request(src, function(res) {
+    return res.headers["content-type"].match(regex);
+  });
+
+  return contentType;
+}
