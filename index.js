7f5a31180cc4272e809b3e2cfd791f3bcfb53b74
davidberget@gmail.com <davidberget@gmail.com>
2020-02-26 12:02:00 -0600

    git

---
+++ index.js
@@ -0,0 +1,12 @@
+const { name } = require("./package.json");
+const path = require("path");
+
+module.exports = (job, settings, options, type) => {
+  return new Promise((resolve, reject) => {
+    job.assets.forEach(asset => {
+      console.log(asset.src);
+    });
+
+    resolve(job);
+  });
+};
