77e3f655f92ac070b858ac41aced123e522f1c64
davidberget@gmail.com <davidberget@gmail.com>
2020-02-26 12:19:05 -0600

    Adding rename

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const { name } = require("./package.json");
 const path = require("path");
+const fs = require("fs");
 
 module.exports = (job, settings, options, type) => {
+  let ext = (options && options.ext) || "png";
+
   return new Promise((resolve, reject) => {
     job.assets
       .filter(asset => asset.type === "image")
-      .forEach(asset => {
-        console.log(path.extname(asset.dest));
+      .forEach(image => {
+        if (path.extname(image.dest) === "") {
+          let newFileName = `${asset.dest}.${ext}`;
+          image.dest = newFileName;
+          fs.rename(asset.dest, newFileName);
+        }
+
+        console.log(job.assets);
       });
 
     resolve(job);
